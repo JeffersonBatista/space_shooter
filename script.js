82d7dc2f79dc81e04e68beb77304b4db54418723
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 22:29:30 -0300

    css final + js move down and shooter

--- script.js
+++ script.js
@@ -26,3 +26,34 @@ function moveUp() {
         yourShip.style.top = `${position}px`;
     }
 }
+
+//função de descer
+function moveDown() {
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+    if(topPosition === "510px"){
+        return
+    } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+//função de atirar
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+//função de carregar laser
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    return newLaser;
+}
