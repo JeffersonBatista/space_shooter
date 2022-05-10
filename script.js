5a4422f7be788dc0597494194d42ffc8db19d8e7
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 22:39:29 -0300

    enemy move

--- script.js
+++ script.js
@@ -93,3 +93,20 @@ function createAliens() {
     playArea.appendChild(newAlien);
     moveAlien(newAlien);
 }
+
+//função para movimentar os inimigos
+function moveAlien(alien) {
+    let moveAlienInterval = setInterval(() => {
+        let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'));
+        if(xPosition <= 50) {
+            if(Array.from(alien.classList).includes('dead-alien')) {
+                alien.remove();
+            } else {
+                gameOver();
+            }
+        } else {
+            alien.style.left = `${xPosition - 4}px`;
+        }
+    }, 30);
+}
+
